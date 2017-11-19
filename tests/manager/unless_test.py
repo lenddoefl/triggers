3498c1fb6f44a7fe4490de0253a4529f356b198a
Phoenix Zerin <phx@phx.ph>
2017-11-19 13:02:14 +1300

    Improved test coverage.

--- tests/manager/unless_test.py
+++ tests/manager/unless_test.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, division, print_function, \
 
 from unittest import TestCase
 
+from triggers import TriggerStorageBackend
 from triggers.manager import TriggerManager, trigger_managers
 from triggers.runners import ThreadingTaskRunner
 from triggers.storage_backends.base import storage_backends
@@ -41,9 +42,16 @@ class TriggerManagerUnlessTestCase(TriggerManagerTestCaseMixin, TestCase):
         ThreadingTaskRunner.join_all()
         self.assertInstanceAbandoned('t_alpha#0')
 
+        # Abandoned tasks are considered resolved.
+        self.assertUnresolvedTasks([])
+        self.assertUnresolvedInstances([])
+
         self.manager.fire('__sessionFinalized')
         ThreadingTaskRunner.join_all()
         self.assertInstanceAbandoned('t_alpha#0')
+
+        self.assertUnresolvedTasks([])
+        self.assertUnresolvedInstances([])
 
     def test_abandoned_task_complex(self):
         """
@@ -64,20 +72,32 @@ class TriggerManagerUnlessTestCase(TriggerManagerTestCaseMixin, TestCase):
         # abandoned.
         self.assertInstanceUnstarted('t_alpha#0')
 
+        self.assertUnresolvedTasks(['t_alpha'])
+        self.assertUnresolvedInstances(['t_alpha#0'])
+
         self.manager.fire('__sessionFinalized')
         ThreadingTaskRunner.join_all()
         self.assertInstanceUnstarted('t_alpha#0')
+
+        self.assertUnresolvedTasks(['t_alpha'])
+        self.assertUnresolvedInstances(['t_alpha#0'])
 
         self.manager.fire('e_bureauCheck')
         ThreadingTaskRunner.join_all()
         # Instance is now abandoned.
         self.assertInstanceAbandoned('t_alpha#0')
 
+        self.assertUnresolvedTasks([])
+        self.assertUnresolvedInstances([])
+
         self.manager.fire('dataReceived')
         ThreadingTaskRunner.join_all()
         self.assertInstanceAbandoned('t_alpha#0')
 
-    def test_abandoned_task_allowing_multiple(self):
+        self.assertUnresolvedTasks([])
+        self.assertUnresolvedInstances([])
+
+    def test_abandoned_task_with_and_every(self):
         """
         Configuring abandonment criteria for a task that can run
         multiple times.
@@ -104,6 +124,9 @@ class TriggerManagerUnlessTestCase(TriggerManagerTestCaseMixin, TestCase):
         self.assertInstanceAbandoned('t_alpha#1')
         self.assertInstanceAbandoned('t_alpha#2')
 
+        self.assertUnresolvedTasks([])
+        self.assertUnresolvedInstances([])
+
     def test_abandoned_task_after_starting(self):
         """
         A task instance's abandonment criteria are satisfied after the
@@ -120,19 +143,35 @@ class TriggerManagerUnlessTestCase(TriggerManagerTestCaseMixin, TestCase):
 
         self.manager.fire('creditsDepleted')
         self.manager.fire('_finishStep')
+
+        # This satisfies the ``unless`` condition.
         self.manager.fire('__deviceDisabled')
+
+        # This creates a new instance of the task, after the ``unless``
+        # condition has been satisfied.
         self.manager.fire('_finishStep')
+
         ThreadingTaskRunner.join_all()
 
         # The first instance ran before `__deviceDisabled` fired, so it
         # is not marked as abandoned.
         self.assertInstanceFinished('t_alpha#0', {})
+
+        # The second instance loaded its ``unless`` state from the
+        # task configuration, so it was marked as abandoned as soon as
+        # it was created.
         self.assertInstanceAbandoned('t_alpha#1')
+
+        self.assertUnresolvedTasks([])
+        self.assertUnresolvedInstances([])
 
     def test_abandoned_clone(self):
         """
         Creating a copy of an existing task instance should also copy
         its abandon state.
+
+        This is a variation on the previous test, using a lower-level
+        mechanism to create the instance.
         """
         self.manager.update_configuration({
             't_alpha': {
@@ -148,13 +187,19 @@ class TriggerManagerUnlessTestCase(TriggerManagerTestCaseMixin, TestCase):
         # abandoned.
         self.assertInstanceUnstarted('t_alpha#0')
 
+        self.assertUnresolvedTasks(['t_alpha'])
+        self.assertUnresolvedInstances(['t_alpha#0'])
+
         # Create a copy of the instance.
-        with self.manager.storage.acquire_lock() as writable_storage: # type: CacheBackend
+        with self.manager.storage.acquire_lock() as writable_storage: # type: TriggerStorageBackend
             writable_storage.clone_instance(writable_storage['t_alpha#0'])
             writable_storage.save()
 
         # Quick sanity check.
         self.assertInstanceUnstarted('t_alpha#1')
+
+        self.assertUnresolvedTasks(['t_alpha'])
+        self.assertUnresolvedInstances(['t_alpha#0', 't_alpha#1'])
 
         self.manager.fire('e_bureauCheck')
         ThreadingTaskRunner.join_all()
@@ -162,3 +207,6 @@ class TriggerManagerUnlessTestCase(TriggerManagerTestCaseMixin, TestCase):
         # The cloned instance inherited its parent's abandon state.
         self.assertInstanceAbandoned('t_alpha#0')
         self.assertInstanceAbandoned('t_alpha#1')
+
+        self.assertUnresolvedTasks([])
+        self.assertUnresolvedInstances([])
